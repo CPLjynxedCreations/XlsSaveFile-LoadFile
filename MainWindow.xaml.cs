ab3cf0a1c4a6daf353b5f685da407d3c6505a4a9
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-10 15:27:10 +1000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ClosedXML;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace XlsSaveFile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Sample Sheet");
+                worksheet.Cell("A1").Value = "Hello World!";
+                worksheet.Cell("A2").FormulaA1 = "=MID(A1, 7, 5)";
+                workbook.SaveAs("HelloWorld.xlsx");
+            }
+            using var wbook = new XLWorkbook("HelloWorld.xlsx");
+
+            var ws1 = wbook.Worksheet(1);
+            var data = ws1.Cell("A1").GetValue<string>();
+
+            Debug.WriteLine(data);
+        }
+
+    }
+}
