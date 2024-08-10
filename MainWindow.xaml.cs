3cb1f5f1d304fa7e600d9b99b2df3616950a8374
CPLjynxedCreations <CPLjynxedCreations@outlook.com>
2024-08-10 16:51:10 +1000

    WOKRS. Makes and loads excel and changes label text from data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Shapes;
 using ClosedXML;
 using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Spreadsheet;
+using DocumentFormat.OpenXml.VariantTypes;
 
 namespace XlsSaveFile
 {
@@ -27,18 +28,44 @@ namespace XlsSaveFile
             InitializeComponent();
             using (var workbook = new XLWorkbook())
             {
-                var worksheet = workbook.Worksheets.Add("Sample Sheet");
-                worksheet.Cell("A1").Value = "Hello World!";
-                worksheet.Cell("A2").FormulaA1 = "=MID(A1, 7, 5)";
-                workbook.SaveAs("HelloWorld.xlsx");
+                var worksheet = workbook.Worksheets.Add("Product Sheet");
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    string productXLS = "A";
+                    string priceXLS = "B";
+                    worksheet.Cell(productXLS + i).Value = "EmptyItem" + i;
+                    worksheet.Column(productXLS).AdjustToContents();
+                    worksheet.Cell(priceXLS + i).Value = "0";
+                    worksheet.Cell(priceXLS + i).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+                }
+                workbook.SaveAs("ProductTest.xlsx");
             }
-            using var wbook = new XLWorkbook("HelloWorld.xlsx");
+            var workBook = new XLWorkbook("ProductTest.xlsx");
+            var workSheet = workBook.Worksheet("Product Sheet");
+            //var ws1 = wbook.Worksheet(1);
+            for (int i = 1; i <= 10; i++)
+            {
+                string productXLS = "A";
+                var data = workSheet.Cell(productXLS + i).GetValue<string>();
+                //Debug.WriteLine(data);
 
-            var ws1 = wbook.Worksheet(1);
-            var data = ws1.Cell("A1").GetValue<string>();
 
-            Debug.WriteLine(data);
+                string setLabel = "lbl" + i;
+                foreach (UIElement item in grTest.Children)
+                {
+                    if (item.GetType() == typeof(TextBlock))
+                    {
+                        TextBlock txtBlock = (TextBlock)item;
+                        if (txtBlock.Name == setLabel)
+                        {
+                            txtBlock.Text = data;
+                        }
+                    }
+                }
+            }
         }
 
     }
 }
+
